acc1e56a8d080e636c7adc53c5ef39b54dee8b0f
AntonioOI17 <121041235@upq.edu.mx>
2025-05-16 17:20:53 -0600

    Ejercicio 2- cuadrado
    cuadrado de un numero.

--- Practica1/Variables.js
+++ Practica1/Variables.js
@@ -9,3 +9,15 @@ let saludo = "Hola, " + nombre + ". Tienes " + edad + " años";
 console.log(saludo);
 
 
+//segundo ejercicio
+
+
+const cuadrado2 = (num) => {
+    return num * num;
+}
+console.log(cuadrado2(3));
+console.log(cuadrado2(8));
+console.log(cuadrado2(6));
+
+//tercer ejercicio
+
